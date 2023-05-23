f581219d1d46eec7578ef0f103b6c90c0bc7be3e
ishrar123 <ishrar145@gmail.com>
2023-05-23 09:34:05 +0530

    commiting the second code

---
+++ src/test/java/first.java
@@ -0,0 +1,6 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("this is a file shared to the git");
+        System.out.println("again changing");
+    }
+}
